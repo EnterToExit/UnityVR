23b4ce323d8d1cc2928ab43758c8c4b0e96e352b
EnterToExit <osmiumsdk@gmail.com>
2022-06-28 22:10:26 +0300

    Added Main Menu

---
+++ VR Test/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartButton()
+    {
+        // SceneManager.LoadScene("NEED TO BE REPLACED BY MAIN SCENE");
+        Debug.Log("switched to new scene");
+    }
+}
